1baed88119713ee7d5ac22aa7dbe7c2f2b87f5fb
CityGuides <86694255+CityGuides@users.noreply.github.com>
2021-06-30 03:04:38 +0300

    Update load_info.php

--- load_info.php
+++ load_info.php
@@ -1,7 +1,1 @@
-<?php
-
-$data = array('names' => 'ShangriLa', 'names' => 'FirstCasino', 'names' => 'FirstCasino');
-header('Content-type: text/javascript');
-echo json_encode($data);
-
-?>
+[{"names":[{ShangriLa, FirstCasino, FirstCasino]}]
